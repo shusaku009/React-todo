df389c451453800ffa4203d7bd78e23d3a3f3b1a
shu009 <105494980+shusaku009@users.noreply.github.com>
2023-09-18 11:03:36 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,15 @@ export const App = () => {
     setIncompleteTodos(newTodos);
   }
 
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  }
+
   return(
     <>
       <div className="input-area">
@@ -34,8 +43,8 @@ export const App = () => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
-                <button onClick={() => {onClickDelete(index)} }>削除</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
+                <button onClick={() => onClickDelete(index)} >削除</button>
               </div>
             );
           })}
